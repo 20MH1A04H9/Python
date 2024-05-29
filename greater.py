70721186461dbfb475b05dadf0bc2010b6ddfa98
VISWA <109515505+20MH1A04H9@users.noreply.github.com>
2024-05-29 23:17:04 +0530

    Update

---
+++ greater.py
@@ -0,0 +1,14 @@
+def maximum(n1,n2,n3):
+    if(n1>n2):
+        if(n1>n3):
+            print( n1, +"is greatest number")
+        else:
+            print(n3,+"is graetest number")
+    
+    else:
+        if(n2>n3):
+            print( n2, "is greatest number")
+        else:
+            print(n3, "is graetest number")
+    
+a= maximum(23,45,67)
